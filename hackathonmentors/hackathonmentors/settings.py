431198238af598d9d6bc9822b4924a5150e1bf21
Hun Jae Lee <hunjael@gmail.com>
2020-04-20 19:19:52 -0400

    :snake: urlparse is so Python 2.7, use urllib.parse like 3.8.2

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import urlparse
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
